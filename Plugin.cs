20f3e205196693834b56c1a0f6bfd2ea03731430
devwhitty <81720436+developer9998@users.noreply.github.com>
2022-05-08 00:57:14 -0230

    initial release

--- Plugin.cs
+++ Plugin.cs
@@ -4,86 +4,51 @@ using UnityEngine;
 using Utilla;
 using System.Reflection;
 using System.IO;
-using UnityEngine.XR;
+using System.ComponentModel;
 
 namespace KabobOrKebob
 {
-	/// <summary>
-	/// This is your mod's main class.
-	/// </summary>
-
 	/* This attribute tells Utilla to look for [ModdedGameJoin] and [ModdedGameLeave] */
-	[ModdedGamemode]
-	[BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+	[Description("HauntedModMenu")]
 	[BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+	[BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+	[ModdedGamemode]
 	public class Plugin : BaseUnityPlugin
 	{
-		bool inRoom;
+		/*Original code and model by Husky#9424*/
+		/*Mod under the MIT license, if you reproduce please credit*/
+
+		/*Assetloading*/
 		public static readonly string assemblyLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-		GameObject kabobl;
-		GameObject handl;
+		static GameObject Kabob; // the kabob item
+		static GameObject Hand; // the player's left hand
 
 		void OnEnable()
 		{
-			/* Set up your mod here */
-			/* Code here runs at the start and whenever your mod is enabled*/
-			HarmonyPatches.ApplyHarmonyPatches();
 			Utilla.Events.GameInitialized += OnGameInitialized;
+			Kabob.SetActive(true);
 		}
 
 		void OnDisable()
 		{
-			/* Undo mod setup here */
-			/* This provides support for toggling mods with ComputerInterface, please implement it :) */
-			/* Code here runs whenever your mod is disabled (including if it disabled on startup)*/
-			HarmonyPatches.RemoveHarmonyPatches();
 			Utilla.Events.GameInitialized -= OnGameInitialized;
+			Kabob.SetActive(false);
 		}
-		
 
 		void OnGameInitialized(object sender, EventArgs e)
 		{
-			/* Code here runs after the game initializes (i.e. GorillaLocomotion.Player.Instance != null) */
+			Hand = GameObject.Find("OfflineVRRig/Actual Gorilla/rig/body/shoulder.L/upper_arm.L/forearm.L/hand.L/palm.01.L/");
+
 			Stream str = Assembly.GetExecutingAssembly().GetManifestResourceStream("KabobOrKebob.Assets.kabob knife");
 			AssetBundle bundle = AssetBundle.LoadFromStream(str);
 			GameObject knife = bundle.LoadAsset<GameObject>("KABOB KNIFE");
-			Instantiate(knife);
 
-			knife.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
-			// left hand kabob
-			kabobl = GameObject.Find("KABOB KNIFE(Clone)");
-			handl = GameObject.Find("OfflineVRRig/Actual Gorilla/rig/body/shoulder.L/upper_arm.L/forearm.L/hand.L/palm.01.L/");
-			kabobl.transform.parent = handl.transform;
-			kabobl.transform.localPosition = new Vector3(-0.1f, 0f, 0.08f);
-			kabobl.transform.rotation = Quaternion.Euler(0f, 80f, 0f);
-			kabobl.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
-			/* Code here runs after the game initializes (i.e. GorillaLocomotion.Player.Instance != null) */
-		}
-		void Update()
-		{
-			// future stuff bool hidecosmetic;
-			// future stuff InputDevices.GetDeviceAtXRNode(XRNode.RightHand).TryGetFeatureValue(CommonUsages.secondaryButton, out hidecosmetic);
-
-		}
-
-		/* This attribute tells Utilla to call this method when a modded room is joined */
-		[ModdedGamemodeJoin]
-		public void OnJoin(string gamemode)
-		{
-			/* Activate your mod here */
-			/* This code will run regardless of if the mod is enabled*/
-
-			inRoom = true;
-		}
-
-		/* This attribute tells Utilla to call this method when a modded room is left */
-		[ModdedGamemodeLeave]
-		public void OnLeave(string gamemode)
-		{
-			/* Deactivate your mod here */
-			/* This code will run regardless of if the mod is enabled*/
-
-			inRoom = false;
+			Kabob = Instantiate(knife);
+			Kabob.transform.SetParent(Hand.transform, false);
+			Kabob.transform.localPosition = new Vector3(-0.1f, 0f, 0.08f);
+			Kabob.transform.localRotation = Quaternion.Euler(0f, 80f, 0f);
+			Kabob.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+			Kabob.SetActive(this.enabled);
 		}
 	}
 }
--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # KabobOrKebob
-KabobOrKebob is a holdable Kebob Knife For Gorilla Tag
-Also I Really Dont Know Is It KabobOrKebob lol
+KabobOrKebob is a holdable Kabob/Kebob Knife For Gorilla Tag
 
-
+Preview:
 ![ioKecFZOUy](https://user-images.githubusercontent.com/103238785/167223549-6cb8ba0e-db72-437a-9c69-4e0edb99fcd3.png)
